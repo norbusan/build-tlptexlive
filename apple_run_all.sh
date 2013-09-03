dfd7abe47e7f8bb6484b74adb01bd40d807e3919
Hironori Kitagawa <h_kitagawa2001@yahoo.co.jp>
2013-09-03 14:35:53 +0900

    external ctie (from munepi, 20130902)

--- apple_run_all.sh
+++ apple_run_all.sh
@@ -1,18 +1,17 @@
 #!/bin/bash -x
 
-svndate=20130106
+svndate=20130902
 
-rm -rf texlive-source-${svndate}
-
-for x in x86_64 i386 ppc; do 
+for x in x86_64 i686 ppc; do 
+    rm -rf texlive-source-${svndate}
     tar -xf ../texlive-source-${svndate}.tar.xz
 
-    BUILD_ARCH=${x} MAKEFLAGS="-j4" BUILD_NORSYNC=1 BUILD_SOURCE=$(pwd)/texlive-source-${svndate} ./build-ptexlive-binaries --macosx
-    rm -rf texlive-source-${svndate}
+    BUILD_XCODEROOT=/Xcode3 BUILD_ARCH=${x} MAKEFLAGS="-j4" BUILD_NORSYNC=1 BUILD_SOURCE=$(pwd)/texlive-source-${svndate} ./build-ptexlive-binaries --macosx 2>&1 | tee ${x}-darwin.log
 done
+rm -rf texlive-source-${svndate}
 
 ./make-universal-darwin.sh
-cp -a /tmp/binaries.zip ../binaries-darwin-${svndate}.zip
+cp -a /tmp/binaries.zip ../binaries-darwin-20130815.${svndate}.zip
 rm -f /tmp/binaries.zip
 
 exit
---
+++ make-universal-darwin.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# This script makes universal binaries of i686 and ppc. 
+
+pwd=$(pwd)
+
+release=$(uname -r)
+arch=universal-darwin${release}
+
+rm -rf /tmp/Work
+mkdir -p /tmp/Work
+cd /tmp/Work
+unzip /tmp/binaries.zip
+[ -d i686-apple-darwin${release} ] || exit 1
+mkdir -p $arch
+
+for x in i686-apple-darwin${release}/*; do
+    [ -d $x ] && continue
+
+    ## for Mach-O executable i386
+    file $x | grep -q -e "Mach-O executable i386"
+    if [ $? -eq 0 -a ! -h $x ]; then
+	lipo -create \
+	    -arch i686 $x \
+	    -arch ppc powerpc-apple-darwin${release}/$(basename $x) \
+	    -output ${arch}/$(basename $x)
+    else
+	## for non-binaries
+	if [ -h $x ]; then
+	    _lnk=$(readlink $x)
+	    (cd ${arch}
+		ln -sf $_lnk $(basename $x)
+	    )
+	else
+	    cp -p $x ${arch}/
+	fi
+    fi
+done
+
+zip -r /tmp/binaries.zip ${arch}/*
+
+rm -rf /tmp/Work
+
+echo "$(basename $0): done."
+
+exit
